43614b33b9f60b1d7c47ae32d0281e96ca88daf7
Marc <marc.gallego@clicko.es>
2024-07-10 18:20:14 +0200

    carrusel listo

--- resources/views/layouts/styles.blade.php
+++ resources/views/layouts/styles.blade.php
@@ -3,13 +3,11 @@
 <link rel="stylesheet" href="{{ asset('css/fonts.css?version='.config('app.version')) }}"/>
 <link rel="stylesheet" href="{{ asset('css/theme.css?version='.config('app.version')) }}"/>
 <link rel="stylesheet" href="{{ asset('css/custom.css?version='.config('app.version')) }}"/>
-<link rel="stylesheet" href="{{ asset('css/custom-mediaquery.css?version='.config('app.version')) }}"/>
 <link rel="stylesheet" href="{{ asset('css/animate.min.css?version='.config('app.version')) }}"/>
+{{-- DECLARO EL SWIPPER ANTES PARA QUE LO DETECTE PARA LOS SLIDES --}}
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
 @yield('styles')
 
-{{-- DECLARO EL SWIPPER ANTES PARA QUE LO DETECTE PARA LOS SLIDES --}}
-<link
-    rel="stylesheet"
-    href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
-/>
-<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+<link rel="stylesheet" href="{{ asset('css/custom-mediaquery.css?version='.config('app.version')) }}"/>
