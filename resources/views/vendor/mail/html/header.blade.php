2a72903015054332538f30a0cd6e2a7269cc2f20
Oscar <oscar.buitrago@clicko.es>
2024-07-29 12:36:57 +0200

    logo del mail cambiado

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ env('APP_URL'). 'img/logo.webp?version='.rand() }}" class="logo" alt="Coppa Cocktail" width="150">
+<img src="{{ env('APP_URL'). 'img/logo.webp?version='.rand() }}" class="logo" alt="Coppa Cocktail">
 </a>
 </td>
 </tr>
